630ffe3788e53c48ac795daf753f65afb85791ec
alain <alainbloch@gmail.com>
2008-09-14 02:37:53 -0700

    Removed Authenticated System requirement.

--- lib/quick_tickets_controller.rb
+++ lib/quick_tickets_controller.rb
@@ -1,6 +1,4 @@
 class QuickTicketsController < ActionController::Base
-  include AuthenticatedSystem
-  before_filter :login_required
   
   def index
     redirect_back_or_default("/")
@@ -15,9 +13,7 @@ class QuickTicketsController < ActionController::Base
     
     if not params[:body].blank? and not params[:title].blank?
       body = params[:body]
-      if self.respond_to?(:logged_in?) and logged_in?
-        body += "\n\nCreated from the web by #{current_user.login} #{current_user.email}"
-      end
+      body += "\n\nCreated from the web by #{params[:user]} #{params[:email}}"
       body += "\nURL: #{params[:url]}"
       redirect_url = "/"
       redirect_url = params[:url] unless params[:url] == "/create_ticket"
@@ -29,7 +25,7 @@ class QuickTicketsController < ActionController::Base
       url = params[:url]
       controller = url.slice(1..((url.index("/", 1) || 0) - 1))
       ticket.tags << controller unless controller.blank?
-      ticket.tags << "#{current_user.login}"
+      ticket.tags << "#{params[:user]}"
       ticket.tags << RAILS_ENV
       if ticket.save
         flash[:notice]="Your ticket was successfully posted, we will try to fix it."
